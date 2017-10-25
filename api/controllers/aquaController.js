3d3aaecc513b6ee41fbb29c526c75a205651e394
Yevhenii Kubiuk <yevhenii.kubiuk@gmail.com>
2017-10-25 11:31:54 +0300

    Added validation functions

--- api/controllers/aquaController.js
+++ api/controllers/aquaController.js
@@ -3,15 +3,13 @@
 var processor = require('./processor');
 
 exports.processInfo = function(req, res) {
-   var params = JSON.parse(req.params.info);
-   var validParams = processor.checkParams(params);
+  var params = req.body;
+  var validParams = processor.checkParams(params);
 
-   //check for required params
-   if(!validParams.hasOwnProperty('weight')){
-      res.status(400).send("Error: weight param is missing");
-   }else {
-      res.status(200).json(validParams);
-   }
-
+  if(!processor.checkForRequired(validParams)){
+    res.status(400).json({Error : "No required params were found"});
+  }else {
+    res.status(200).json(validParams);
+  }
 
 };
--- api/controllers/processor.js
+++ api/controllers/processor.js
@@ -1,10 +1,29 @@
 function processor() {}
 
 processor.stats = [
-   {name : 'weight', required : true},
-   {name : 'age', required : false},
-   {name : 'climateZone', required : false},
-   {name : 'sportCondition', required : false}];
+   {name : 'weight', required : true, description: 'Unsigned Float'},
+   {name : 'age', required : false, description: 'Unsinged Integer'},
+   {name : 'climateZone', required : false, description : 'Integer between [0 .. 2]'},
+   {name : 'sportCondition', required : false, description : 'Integer between [0 .. 2]'}];
+
+processor.uFloatValidation = function(value) {
+  if(typeof value === 'number' && Number.isFinite(value)){
+    if(value >= 0){
+      value.toPrecision(5);
+      return value;
+    }
+  }
+  return NaN;
+}
+
+processor.uIntegerValidation = function(value) {
+  if(typeof value === 'number' && Number.isFinite(value)){
+    if(value >= 0){
+      return value;
+    }
+  }
+  return NaN;
+}
 
 processor.checkParams = function(params) {
    var validParams = {};
@@ -15,5 +34,20 @@ processor.checkParams = function(params) {
    }
    return validParams;
 };
+
+processor.checkForRequired = function (params) {
+  for (stat in processor.stats){
+    if (processor.stats[stat].required){
+      if(params[processor.stats[stat].name] == null){
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+processor.validateParams = function (params) {
+  return params;
+}
 
 module.exports = processor;
